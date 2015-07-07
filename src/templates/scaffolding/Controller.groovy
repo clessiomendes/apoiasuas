cfb003189cd691bff508be76ee19f1c4fe288f1b
Clessio <clessiomendes@yahoo.com>
2015-07-06 23:39:37 -0300

    Corrigindo definições de segurança e nomes de variáveis geradas no scaffolding do Controller

--- src/templates/scaffolding/Controller.groovy
+++ src/templates/scaffolding/Controller.groovy
@@ -1,30 +1,38 @@
 <%=packageName ? "package ${packageName}\n\n" : ''%>
 
+
+import grails.plugin.springsecurity.annotation.Secured
+import org.apoiasuas.seguranca.DefinicaoPapeis
+
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+@Secured([DefinicaoPapeis.USUARIO])
 class ${className}Controller {
 
     static defaultAction = "list"
+
+    @Secured([DefinicaoPapeis.USUARIO_LEITURA])
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        render view: "list", model:[${propertyName}InstanceList:${className}.list(params), ${propertyName}Count: ${className}.count()]
+        render view: "list", model:[${propertyName}List:${className}.list(params), ${propertyName}Count: ${className}.count()]
     }
 
+    @Secured([DefinicaoPapeis.USUARIO_LEITURA])
     def show(${className} ${propertyName}) {
         if (! ${propertyName})
             return notFound()
-        render view: "show", model: [${propertyName}Instance:${propertyName}Instance]
+        render view: "show", model: [${propertyName}:${propertyName}]
     }
 
     def create() {
-        render view:"create", model: [${propertyName}Instance:new ${className}(params)]
+        render view:"create", model: [${propertyName}:new ${className}(params)]
     }
 
     def edit(${className} ${propertyName}) {
         if (! ${propertyName})
             return notFound()
-        render view:"edit", model: [${propertyName}Instance:${propertyName}Instance]
+        render view:"edit", model: [${propertyName}:${propertyName}]
     }
 
     def save(${className} ${propertyName}) {
@@ -35,24 +43,25 @@ class ${className}Controller {
             return
         }
 
-        if (! $implementarServico$.grava(${propertyName})) {
+        if (! #implementarServico.grava(${propertyName})) {
             //exibe o formulario novamente em caso de problemas na validacao
             return render(view: modoCriacao ? "create" : "edit" , model: [${propertyName}:${propertyName}])
         }
         flash.message = message(code: 'default.created.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-        render view: "show", model: [${propertyName}Instance: ${propertyName}Instance]
+        render view: "show", model: [${propertyName}: ${propertyName}]
     }
 
     def delete(${className} ${propertyName}) {
         if (! ${propertyName})
             return notFound()
 
-        $implementarServico$.apaga(${propertyName})
+        #implementarServico.apaga(${propertyName})
 
         flash.message = message(code: 'default.deleted.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
         redirect action:"list"
     }
 
+    @Secured([DefinicaoPapeis.USUARIO_LEITURA])
     protected def notFound() {
         flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
         redirect action: "list"
--- src/templates/scaffolding/create.gsp
+++ src/templates/scaffolding/create.gsp
@@ -29,7 +29,7 @@
 					<g:render template="form"/>
 				</fieldset>
 				<fieldset class="buttons">
-					<g:submitButton name="create" class="save" value="\${message(code: 'default.button.create.label', default: 'Create')}" />
+					<g:submitButton name="create" class="save" value="\${message(code: 'default.button.create.label', default: 'Novo')}" />
 				</fieldset>
 			</g:form>
 		</div>
