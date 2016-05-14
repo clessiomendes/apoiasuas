66630d584edc4ac2fd0ac4c61229989b6f31b497
Clessio <clessiomendes@yahoo.com>
2016-05-13 23:20:53 -0300

    Corrigindo bug repentino (mal explicado) de "pacote nao encontrado"

--- src/groovy/org/apoiasuas/anotacoesDominio/InfoPropriedadeDominio.groovy
+++ src/groovy/org/apoiasuas/anotacoesDominio/InfoPropriedadeDominio.groovy
@@ -1,7 +1,5 @@
 package org.apoiasuas.anotacoesDominio
 
-import org.apoiasuas.formulario.CampoFormulario
-
 import java.lang.annotation.ElementType
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
@@ -10,11 +8,11 @@ import java.lang.annotation.Target
 @Retention(RetentionPolicy.RUNTIME)
 @Target([ElementType.FIELD])
 @interface InfoPropriedadeDominio {
-    static final CampoFormulario.Tipo TIPO_DEFAULT = CampoFormulario.Tipo.TEXTO
+    static final org.apoiasuas.formulario.CampoFormulario.Tipo TIPO_DEFAULT = org.apoiasuas.formulario.CampoFormulario.Tipo.TEXTO
     static final int TAMANHO_DEFAULT = 60
     String codigo()
     String descricao() default ''
-    CampoFormulario.Tipo tipo() default CampoFormulario.Tipo.TEXTO
+    org.apoiasuas.formulario.CampoFormulario.Tipo tipo() default org.apoiasuas.formulario.CampoFormulario.Tipo.TEXTO
     int tamanho() default 60
     boolean atualizavel() default true
 }
