f1c8e74c0f53f76731a37c6f2acf726fb3f6ecc8
clessiomendes <clessiomendes@yahoo.com>
2017-04-24 01:16:48 -0300

    deploy validacao

--- scripts/build-valid.bat
+++ scripts/build-valid.bat
@@ -1,9 +1,9 @@
-cd ..
-echo Inicio: %time%
-call grails clean-all --non-interactive --refresh-dependencies
-rmdir /s /q .\target
-call grails prod war --stacktrace -Dorg.apoiasuas.datasource=CLEVERCLOUD_POSTGRES_VALID -Dgrails.project.work.dir=tmp_deploy
-copy .\target\apoiasuas-0.1.war ..\deploy-cc\valid\apoiasuas.war
+--cd ..
+--echo Inicio: %time%
+--call grails clean-all --non-interactive --refresh-dependencies
+--rmdir /s /q .\target
+--call grails prod war --stacktrace -Dorg.apoiasuas.datasource=CLEVERCLOUD_POSTGRES_VALID -Dgrails.project.work.dir=tmp_deploy
+copy .\target\apoiasuas-0.1.war c:\workspaces\deploy-cc\valid\apoiasuas.war
 cd ..\deploy-cc\valid\
 git add .
 git commit -m "deploy validacao"
